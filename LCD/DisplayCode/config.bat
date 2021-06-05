da366a5dbc7a294cff2ee2770822e9f4cf06209d
Victor Henrique Ventura Freitas <79233369+VhCompany1@users.noreply.github.com>
2021-06-05 17:32:07 -0300

    1.6.16

--- LCD/DisplayCode/config.bat
+++ LCD/DisplayCode/config.bat
@@ -1,3 +1,8 @@
+:: Configurações do DisplayCode
+
+:: Desenvolvedor Oficial: VhCompany / https://github.com/VhCompany1
+:: Empresa Oficial: VhCloud / https://github.com/Vh-Cloud
+
 set cor=3
 set resulution=1024x1280
 set message=echo
